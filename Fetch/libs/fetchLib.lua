a2c865e8179522c0f27422e484aa19a509b42575
Edoardo Barchiesi <edoardo.barchiesi@hotmail.it>
2021-09-24 01:11:44 +0200

    trasformato fetch in lib

--- Fetch/libs/fetchLib.lua
+++ Fetch/libs/fetchLib.lua
@@ -1,14 +1,16 @@
 _DEBUG = false
- 
+
+---@diagnostic disable-next-line: lowercase-global
+
 function try(f, catch_f)
     local status, exception = pcall(f)
     if not status then
         catch_f(exception)
     end
 end
- 
-function isChest(peripheralName)
- 
+
+function IsChest(peripheralName)
+
     local wrap = peripheral.wrap(peripheralName)
     local result = false
     try(function()
@@ -22,8 +24,8 @@ function isChest(peripheralName)
     end)
     return result
 end 
- 
-function fetch(itemName, _debug)
+
+function Fetch(itemName, _debug)
     if _debug == "true" then
         _DEBUG = true
     end
@@ -35,13 +37,13 @@ function fetch(itemName, _debug)
         local inv = peripheral.wrap(name)
         local itemList
         
-        if isChest(name) then
+        if IsChest(name) then
             itemList = inv.list()            
             --print(itemList)
             --sleep(.5)
             for k, item in pairs(itemList) do
                 local itemDesc = inv.getItemMeta(k)
-                if itemDesc == null then
+                if itemDesc == nil then
                     if _DEBUG then
                         print("Slot n° " .. k .. " was empty")
                     end
@@ -68,4 +70,39 @@ function fetch(itemName, _debug)
     
     return locationList
     
+end
+
+function GetFromFetch( itemName, destination , quantity)
+    
+    local fetched = Fetch(itemName, false)
+    local to_wrap = peripheral.wrap(destination);
+
+    if IsChest(destination) == true then
+        for i, item in ipairs(fetched) do
+            local moved = 0
+            if  destination ~= item.invName then
+                print("source" .. item.invName)
+                local iterations = 0
+                local remaining = 0
+                if quantity > 64 then
+                    iterations = math.floor(quantity / 64)
+                    remaining = math.fmod(quantity, 64)
+                else
+                    remaining = quantity
+                end
+                while (peripheral.wrap(item.invName).getItemMeta(item.position) ~= nil) or ((remaining == 0) and (iterations == 0)) do
+                    
+                    local changed = to_wrap.pullItems(item.invName, item.position)
+                    if changed == 0 or changed == nil then
+                        print("Destination is full")
+                        break
+                    end
+                    remaining = remaining - changed
+                    moved = moved + changed
+                end
+            end
+        end
+    else
+        error(destination .. " is not an available inventory.")
+    end
 end
